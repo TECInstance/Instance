5eefbaaa097341ef7723736a44dd0a90c88ab74e
RWejlgaard <wejlgaard@live.dk>
2015-11-25 12:59:34 +0100

    Contact made

--- Instance/MainWindow.xaml.cs
+++ Instance/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,9 +27,53 @@ namespace Instance
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
+        public TcpClient client = new TcpClient();
 
+        private void button_Click(object sender, RoutedEventArgs e) {
+            var message = IpInput.Text;
+
+            
+            try {
+                client.Connect((IPAddress.Parse(IpInput.Text)), 1337);
+            }
+            catch (Exception) {
+                MessageBox.Show("Invalid IP");
+            }
+
+            if (client.Connected) {
+                ConnectBtn.IsEnabled = false;
+                DisconnectBtn.IsEnabled = true;
+                IpInput.IsEnabled = false;
+
+
+                byte[] buffer = new byte[1000];
+
+                NetworkStream streamclient = client.GetStream();
+                byte[] sendBytes = Encoding.ASCII.GetBytes("");
+
+                streamclient.Write(sendBytes,0,sendBytes.Length);
+                
+            }
+
+            
+        }
+
+        private void ChatInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NetworkStream streamclient = client.GetStream();
+            byte[] sendBytes = Encoding.ASCII.GetBytes(ChatInput.Text);
+
+            streamclient.Write(sendBytes, 0, sendBytes.Length);
+        }
+
+        private void DisconnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            client.Close();
         }
     }
 }
---
+++ InstanceSrv/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceSrv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var srv = new TcpListener(IPAddress.Parse(GetLocalIpAddress()), 1337);
+            srv.Start();
+            while (true) {
+                if (srv.Pending()) {
+                    var client = srv.AcceptTcpClient();
+                    Console.WriteLine("Connected to someone!");
+                    var buffer = new byte[1000];
+                    while (true) {
+                        NetworkStream streamclient = client.GetStream();
+                        streamclient.Read(buffer, 0, buffer.Length);
+                        var datarecieved = System.Text.Encoding.ASCII.GetString(buffer);
+                        Console.WriteLine(datarecieved);
+                    }
+                }
+            }
+        }
+
+        public static string GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)) {
+                return ip.ToString();
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
