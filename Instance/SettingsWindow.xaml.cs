d373fc9738c795d97e14a5a0e8d5ffb3c67dd013
RWejlgaard <wejlgaard@live.dk>
2015-12-10 13:53:02 +0100

    Fixed divider colour changing

--- Instance/SettingsWindow.xaml.cs
+++ Instance/SettingsWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
 using MahApps.Metro;
 
 namespace Instance {
@@ -9,129 +6,108 @@ namespace Instance {
     ///     Interaction logic for SettingsWindow.xaml
     /// </summary>
     public partial class SettingsWindow {
-        public static Brush DivBrush;
-
         public SettingsWindow() {
             InitializeComponent();
-            
-            
         }
 
         private void RedTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Red"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
         private void GreenTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Green"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
         private void BlueTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Blue"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void PurpleTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void PurpleTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Purple"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void OrangeTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void OrangeTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Orange"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void LimeTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void LimeTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Lime"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void EmeraldTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void EmeraldTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Emerald"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void TealTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void TealTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Teal"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void CyanTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void CyanTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Cyan"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void CobaltTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void CobaltTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Cobalt"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void IndigoTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void IndigoTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Indigo"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
-        } 
+            MainWindow.DivBrush = GlowBrush;
+        }
 
-        private void VioletTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void VioletTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Violet"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void PinkTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void PinkTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Pink"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void MagentaTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void MagentaTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Magenta"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void CrimsonTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void CrimsonTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Crimson"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void AmberTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void AmberTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Amber"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void YellowTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void YellowTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Yellow"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void BrownTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void BrownTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Brown"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void OliveTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void OliveTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Olive"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
 
-        private void SteelTile_Clicked(object sender, RoutedEventArgs e)
-        {
+        private void SteelTile_Clicked(object sender, RoutedEventArgs e) {
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Steel"), ThemeManager.GetAppTheme("BaseDark"));
-            DivBrush = GlowBrush;
+            MainWindow.DivBrush = GlowBrush;
         }
     }
 }
