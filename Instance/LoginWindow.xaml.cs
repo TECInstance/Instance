06887f6d4bd8cc537322deb7b30af10718f8c718
nikhunter <niklas@micheelsen.com>
2015-12-14 10:27:27 +0100

    Full cleanup and deletion of unessesary sections

--- Instance/LoginWindow.xaml.cs
+++ Instance/LoginWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace Instance {
     public partial class LoginWindow {
@@ -13,11 +14,11 @@ namespace Instance {
         public bool UsernameRemembrance;
 
         public LoginWindow() {
-            var _test = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
-            if (File.Exists(_test)) {
-                var _rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
-                using (var _readfile = new StreamReader(_rememberfileName)) {
-                    UsernameRemembrance = _readfile.ReadLine() == "true";
+            var test = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
+            if (File.Exists(test)) {
+                var rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
+                using (var readfile = new StreamReader(rememberfileName)) {
+                    UsernameRemembrance = readfile.ReadLine() == "true";
                 }
             }
 
@@ -25,9 +26,9 @@ namespace Instance {
 
             if (UsernameRemembrance) {
                 RememberUserCheck.IsChecked = true;
-                var _userfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RememberedUser.txt");
-                using (var _readfile = new StreamReader(_userfileName)) {
-                    UsernameText.Text = _readfile.ReadLine();
+                var userfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RememberedUser.txt");
+                using (var readfile = new StreamReader(userfileName)) {
+                    UsernameText.Text = readfile.ReadLine();
                 }
             }
             else {
@@ -38,67 +39,65 @@ namespace Instance {
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e) {
             if (RememberUserCheck.IsChecked == true) {
-                var _rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
-                using (var _writefile = new StreamWriter(_rememberfileName)) {
+                var rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
+                using (var writefile = new StreamWriter(rememberfileName)) {
                     //if the file doesn't exist, create it
-                    if (!File.Exists(_rememberfileName)) {
-                        File.Create(_rememberfileName);
+                    if (!File.Exists(rememberfileName)) {
+                        File.Create(rememberfileName);
                     }
 
-                    _writefile.WriteLine("true");
+                    writefile.WriteLine("true");
                 }
 
-                var _userfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RememberedUser.txt");
-                using (var _writefile = new StreamWriter(_userfileName)) {
+                var userfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RememberedUser.txt");
+                using (var writefile = new StreamWriter(userfileName)) {
                     //if the file doesn't exist, create it
-                    if (!File.Exists(_userfileName)) {
-                        File.Create(_userfileName);
+                    if (!File.Exists(userfileName)) {
+                        File.Create(userfileName);
                     }
 
-                    _writefile.WriteLine(UsernameText.Text);
+                    writefile.WriteLine(UsernameText.Text);
                 }
             }
             else if (RememberUserCheck.IsChecked == false) {
-                var _rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
-                using (var _writefile = new StreamWriter(_rememberfileName)) {
+                var rememberfileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UsernameRemembrance.txt");
+                using (var writefile = new StreamWriter(rememberfileName)) {
                     //if the file doesn't exist, create it
-                    if (!File.Exists(_rememberfileName)) {
-                        File.Create(_rememberfileName);
+                    if (!File.Exists(rememberfileName)) {
+                        File.Create(rememberfileName);
                     }
 
-                    _writefile.WriteLine("false");
+                    writefile.WriteLine("false");
                 }
             }
 
             if (AuthenticateLogin(UsernameText.Text, PasswordText.Password)) {
                 LoginSuccess = true;
-                MainWindow.InitializeConnection();
                 Close();
             }
             else {
-                //FIND PRETTIER SOLUTION
-                MessageBox.Show("Username and password does not seem to be valid");
+                this.ShowMessageAsync(":(", "Username and password does not seem to be valid");
             }
         }
 
         public bool AuthenticateLogin(string username, string password) {
-            var _hash = GetHash(password);
+            var hash = GetHash(password);
             const string connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
 
-            using (var _con = new SqlConnection(connectionString)) {
-                var _dt = new DataTable();
+            using (var con = new SqlConnection(connectionString)) {
+                var dt = new DataTable();
                 try {
-                    _con.Open();
+                    con.Open();
                 }
                 catch (Exception) {
                     MessageBox.Show("Connection failed");
                 }
 
-                var _command = new SqlCommand("select * from logins", _con);
-                var _dr = _command.ExecuteReader();
-                _dt.Load(_dr);
-                
-                if (_dt.Rows.Cast<DataRow>().Where(variable => variable.Field<string>("usernames") == username).Any(variable => variable.Field<string>("passwords") == _hash)) {
+                var command = new SqlCommand("select * from logins", con);
+                var dr = command.ExecuteReader();
+                dt.Load(dr);
+
+                if (dt.Rows.Cast<DataRow>().Where(variable => variable.Field<string>("usernames") == username).Any(variable => variable.Field<string>("passwords") == hash)) {
                     MainWindow.Username = UsernameText.Text.ToLower();
                     return true;
                 }
@@ -107,15 +106,15 @@ namespace Instance {
         }
 
         public static string GetHash(string inputString) {
-            using (var _sha1 = new SHA1Managed()) {
-                var _hash = _sha1.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-                var _sb = new StringBuilder(_hash.Length*2);
+            using (var sha1 = new SHA1Managed()) {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+                var sb = new StringBuilder(hash.Length*2);
 
-                foreach (var _b in _hash) {
-                    _sb.Append(_b.ToString("X2"));
+                foreach (var b in hash) {
+                    sb.Append(b.ToString("X2"));
                 }
 
-                return _sb.ToString();
+                return sb.ToString();
             }
         }
 
--- Instance/MainWindow.xaml.cs
+++ Instance/MainWindow.xaml.cs
@@ -1,192 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Instance.Annotations;
-using MahApps.Metro;
-using MahApps.Metro.Controls;
 
-namespace Instance
-{
-    public partial class MainWindow
-    {
+namespace Instance {
+    public partial class MainWindow {
         public static TcpClient Client = new TcpClient();
         public static bool IsLocked = true;
         public static string InstanceIp = GetLocalIpAddress();
         public static string Username;
         public static string EllipseColour = "#FF00FF00";
         public static Brush DivBrush = (Brush) new BrushConverter().ConvertFrom("#E51400");
-        private ObservableCollection<Contact> _ContactList = new ObservableCollection<Contact>();
-        public ObservableCollection<Contact> ContactList => _ContactList;
 
-        public MainWindow()
-        {
-            var _loginWindow = new LoginWindow();
+        public MainWindow() {
+            var loginWindow = new LoginWindow();
             Hide();
-            _loginWindow.Show();
+            loginWindow.Show();
             LoadContacts();
 
             InitializeComponent();
 
-            _loginWindow.Closed += delegate
-            {
-                if (_loginWindow.LoginSuccess)
-                {
+            loginWindow.Closed += delegate {
+                if (loginWindow.LoginSuccess) {
                     Show();
                     DivBrush = GlowBrush;
                 }
-                else
-                {
-                    MessageBox.Show("Unresolved authentication - evaded login fail");
+                else {
                     Close();
                 }
             };
         }
 
-        private void LoadContacts () {
+        public ObservableCollection<Contact> ContactList { get; } = new ObservableCollection<Contact>();
+
+        private void LoadContacts() {
             const string connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
 
-            using (var _con = new SqlConnection(connectionString))
-            {
-                var _dt = new DataTable();
-                try
-                {
-                    _con.Open();
+            using (var con = new SqlConnection(connectionString)) {
+                var dt = new DataTable();
+                try {
+                    con.Open();
                 }
-                catch (Exception)
-                {
+                catch (Exception) {
                     MessageBox.Show("Connection failed");
                 }
 
-                var _command = new SqlCommand("select * from logins", _con);
-                var _dr = _command.ExecuteReader();
-                _dt.Load(_dr);
+                var command = new SqlCommand("select * from logins", con);
+                var dr = command.ExecuteReader();
+                dt.Load(dr);
 
-                foreach (DataRow _row in _dt.Rows) {
-                    if (_row.Field<string>("status") == "Offline")
-                    {
-                        EllipseColour = "#424242";
-                    }
-                    else
-                    {
-                        EllipseColour = "#FF00FF00";
-                    }
+                foreach (DataRow row in dt.Rows) {
+                    EllipseColour = row.Field<string>("status") == "Offline" ? "#424242" : "#FF00FF00";
 
-                    _ContactList.Add(new Contact
-                    {
-                        Name = _row.Field<string>("usernames"),
-                        Title = _row.Field<string>("title"),
+                    ContactList.Add(new Contact {
+                        Name = row.Field<string>("usernames"),
+                        Title = row.Field<string>("title"),
                         Status = EllipseColour
                     });
 
-                    if (_row.Field<string>("status") == "Offline") {
-                        EllipseColour = "#424242";
-                    }
-                    else {
-                        EllipseColour = "#FF00FF00";
-                    }
+                    EllipseColour = row.Field<string>("status") == "Offline" ? "#424242" : "#FF00FF00";
                 }
             }
         }
 
-        public static void InitializeConnection()
-        {
-            try
-            {
-                Client.Connect(IPAddress.Parse(InstanceIp), 1337);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Instance Servers seems to be offline, logging in anyway");
-            }
-
-            if (Client.Connected)
-            {
-                TrySend("INSTANCEINIT " + GetLocalIpAddress() + " " + Username);
-            }
-        }
-
-        private static void TrySend(string str)
-        {
-            try
-            {
-                var _streamclient = Client.GetStream();
-                var _sendBytes = Encoding.ASCII.GetBytes(str);
-
-                _streamclient.Write(_sendBytes, 0, _sendBytes.Length);
-            }
-            catch (Exception)
-            {
-                Debug.WriteLine("Can't connect to server (this is bad)");
-            }
-        }
-
-        public static string GetLocalIpAddress()
-        {
-            var _host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var _ip in _host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork))
-            {
-                return _ip.ToString();
+        public static string GetLocalIpAddress() {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)) {
+                return ip.ToString();
             }
             throw new Exception("Local IP Address Not Found!");
         }
 
-        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var _settingsWindow = new SettingsWindow();
-            _settingsWindow.Show();
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e) {
+            var settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
         }
 
-        private void MetroWindow_Activated(object sender, EventArgs e)
-        {
+        private void MetroWindow_Activated(object sender, EventArgs e) {
             Divider.Background = DivBrush;
         }
 
-        private void MetroWindow_Deactivated(object sender, EventArgs e)
-        {
+        private void MetroWindow_Deactivated(object sender, EventArgs e) {
             Divider.Background = (Brush) new BrushConverter().ConvertFrom("#808080");
         }
 
-        private void ChatInput_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter && ChatInput.Text != "")
-            {
+        private void ChatInput_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter && ChatInput.Text != "") {
                 InsertIntoChat(ChatInput.Text);
                 ChatInput.Text = null;
             }
         }
 
-        private void InsertIntoChat(string str)
-        {
+        private void InsertIntoChat(string str) {
             ChatText.AppendText(str + "\n");
             ChatText.ScrollToEnd();
         }
 
-        private void chatText_TextChanged(object sender, TextChangedEventArgs e)
-        {
+        private void chatText_TextChanged(object sender, TextChangedEventArgs e) {
         }
     }
 
-    public class Contact
-    {
+    public class Contact {
         public string Name { get; set; }
         public string Title { get; set; }
         public string Status { get; set; }
